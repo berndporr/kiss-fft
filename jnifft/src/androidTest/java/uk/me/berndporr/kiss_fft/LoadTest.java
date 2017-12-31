8b3f13a422ccc458f35926e464aba933d29b7db5
Bernd Porr <mail@berndporr.me.uk>
2017-12-31 22:46:47 +0000

    Tidied up dependencies

--- jnifft/src/androidTest/java/uk/me/berndporr/kiss_fft/LoadTest.java
+++ jnifft/src/androidTest/java/uk/me/berndporr/kiss_fft/LoadTest.java
@@ -1,7 +1,5 @@
 package uk.me.berndporr.kiss_fft;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Test;
